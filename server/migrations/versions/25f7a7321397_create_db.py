59004862a00b4d67f4775326b189adf52f3af540
Kamryn Smith <programersign@gmail.com>
2023-09-04 13:40:14 -0700

    added to models

---
+++ server/migrations/versions/25f7a7321397_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 25f7a7321397
+Revises: 
+Create Date: 2023-09-01 14:27:31.665212
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '25f7a7321397'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -3,4 +3,34 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    # add password and serializer
+    posts = db.relationship("Post", backref="user")
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+    genre = db.Column(db.String)
+    # come back to add more
+
+    user_id = db.Column(db.Integer(), db.ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f"<Post {self.genre}>" # come back to repr
+
+class Plant(db.Model):
+    __tablename__ = "plants"
+
+    id = Column(db.Integer, primary_key=True)
+    name = Column(db.String)
+    native = Column(db.String)
+    growingLevel = Column(db.Integer)
