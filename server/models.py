fd2d164330da66f6142e38330d932e379238dc67
Kamryn Smith <programersign@gmail.com>
2023-09-05 11:46:45 -0700

    added forum and plant models and connected relationships

--- server/models.py
+++ server/models.py
@@ -8,11 +8,13 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    # add password and serializer
+    accountType = db.Column(db.String)
+
     posts = db.relationship("Post", backref="user")
+    plants = db.relationship("Plant", secondary=user_plant, back_populates="users")
 
     def __repr__(self):
-        return f"<User {self.username}>"
+        return f"<User {self.username} | {self.accountType}>"
 
 class Post(db.Model):
     __tablename__ = "posts"
@@ -20,17 +22,39 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String)
     genre = db.Column(db.String)
-    # come back to add more
-
     user_id = db.Column(db.Integer(), db.ForeignKey("users.id"))
+    forum_id = db.Column(db.Integer(), db.ForeignKey("forums.id"))
 
     def __repr__(self):
-        return f"<Post {self.genre}>" # come back to repr
+        return f"<Post {self.genre} | {self.user_id}>"
 
 class Plant(db.Model):
     __tablename__ = "plants"
 
-    id = Column(db.Integer, primary_key=True)
-    name = Column(db.String)
-    native = Column(db.String)
-    growingLevel = Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    #native = db.Column(db.String)
+    growingLevel = db.Column(db.Integer) 
+    img = db.Column(db.String)
+
+    users = db.relationship("User", secondary=user_plant, back_populates="plants")
+
+class Forum(db.Model):
+    __tablename__ = "forums"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    followers = db.Column(db.Integer)
+
+    # Maybe add a number of posts
+
+    posts = db.relationship("Post", backref="forum")
+
+    def __repr__(self):
+        return f"<Forum {self.title} | {self.followers}"
+
+user_plant = db.Table(
+    "user_plant", 
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("plant_id", db.Integer, db.ForeignKey("plants.id"))
+)
