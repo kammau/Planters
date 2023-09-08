eef94f5cf483b8046d536fbe7f7bcafc907e6cf3
Kamryn Smith <programersign@gmail.com>
2023-09-08 13:08:56 -0700

    created check session resource and added basics to login resource

--- server/app.py
+++ server/app.py
@@ -20,13 +20,28 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def index():
-    return '<h1>Phase 4 Project Server</h1>'
+class CheckSession(Resource):
+    pass
 
-class Signup(Resource):
-    def 
+class Login(Resource):
+    def post(self):
+        username = request.get_json().get("username")
+        password = request.get_json().get("password")
 
+        user = User.query.filter(User.username == username).first()
+
+        if user and user.authenticate(password):
+            session["user_id"] = user.id
+            response = make_response(
+                user.to_dict(),
+                200
+            )
+            return response
+        
+        return {"error": "401 Unauthorized"}, 401
+
+
+api.add_resource(Login, "/login", endpoint="login")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
