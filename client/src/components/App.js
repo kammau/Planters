99f9fc2252f1ee11f6eb6a6dfef53335dc2acece
Kamryn Smith <programersign@gmail.com>
2023-09-08 12:35:29 -0700

    added password hash setter to models

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import NavBar from "./NavBar";
-import Login from "./Login"
+import Login from "./Login";
+import Forums from "./Forums";
+import PlantCollection from "./PlantCollection";
 
 function App() {
   return (
--- server/models.py
+++ server/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from config import db, bcrypt
 
 user_plant = db.Table(
     "user_plant", 
@@ -14,10 +14,26 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
     accountType = db.Column(db.String) # Maybe get rid of accountType later...
 
     posts = db.relationship("Post", backref="user")
     plants = db.relationship("Plant", secondary=user_plant, back_populates="users")
+
+    # @hybrid_property
+    # def password_hash(self):
+    #     raise AttributeError("")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode("utf-8"))
+        self._password_hash = password_hash.decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode("utf-8"))
+
 
     def __repr__(self):
         return f"<User {self.username} | {self.accountType}>"
