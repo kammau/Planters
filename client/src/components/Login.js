1a797aa3d30cdf182abaa26b278a6c7398720c05
Kamryn Smith <programersign@gmail.com>
2023-09-10 16:13:38 -0700

    added a form element to login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,8 +4,38 @@ function Login({ setUser }) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password })
+        })
+        .then((res) => {
+            if (res.ok) {
+                res.json().then((user) => setUser(user))
+            }
+        })
+    }
+
     return (
-        <h1>Welcome to the Login page!</h1>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h1>Welcome Back!</h1>
+                <p>Please Enter Your Login Info</p>
+                
+                <label for="username">Username</label>
+                <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+
+                <label for="password">Password</label>
+                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                <button type="submit">Login</button>
+            </form>
+        </div>
     )
 }
 
