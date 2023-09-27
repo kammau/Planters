c16d693964774f03269446a243882ca5add33505
Kamryn Smith <programersign@gmail.com>
2023-09-26 17:31:53 -0700

    bcrypt

--- server/config.py
+++ server/config.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 from sqlalchemy_serializer import SerializerMixin
 
 # Local imports
